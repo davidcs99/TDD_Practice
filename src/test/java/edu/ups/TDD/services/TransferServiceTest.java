5ef6f1ae5bbf0fa84d781edaba7f85e5a27640b5
davidcs99 <luis.cs99.dc@gmail.com>
2024-12-29 21:14:42 -0500

    test: TRANSFER_AC2 desc: Should Fail Transfer When Insufficient Funds phase: fail result: C:\Users\luisc\Documents\Maestria\automatizacion de los prrocesos\TDD\TDD\TDD\src\test\java\edu\ups\TDD\services\TransferServiceTest.java:13:13         java: cannot find symbol           symbol:   class TransferService           location: class edu.ups.TDD.services.TransferServiceTest         C:\Users\luisc\Documents\Maestria\automatizacion de los prrocesos\TDD\TDD\TDD\src\test\java\edu\ups\TDD\services\TransferServiceTest.java:18:45         java: cannot find symbol           symbol:   class TransferRequest           location: class edu.ups.TDD.services.TransferServiceTest         C:\Users\luisc\Documents\Maestria\automatizacion de los prrocesos\TDD\TDD\TDD\src\test\java\edu\ups\TDD\services\TransferServiceTest.java:19:9         java: cannot find symbol           symbol:   class TransferRequest           location: class edu.ups.TDD.services.TransferServiceTest         C:\Users\luisc\Documents\Maestria\automatizacion de los prrocesos\TDD\TDD\TDD\src\test\java\edu\ups\TDD\services\TransferServiceTest.java:19:39         java: cannot find symbol           symbol:   class TransferRequest           location: class edu.ups.TDD.services.TransferServiceTest         C:\Users\luisc\Documents\Maestria\automatizacion de los prrocesos\TDD\TDD\TDD\src\test\java\edu\ups\TDD\services\TransferServiceTest.java:22:9         java: cannot find symbol           symbol:   class TransferResult           location: class edu.ups.TDD.services.TransferServiceTestt.java:23:9         java: cannot find symbol           symbol:   class TransferResult           location: class edu.ups.TDD.services.TransferServiceTest

--- src/test/java/edu/ups/TDD/services/TransferServiceTest.java
+++ src/test/java/edu/ups/TDD/services/TransferServiceTest.java
@@ -28,5 +28,19 @@ public class TransferServiceTest {
         assertEquals("Transferencia exitosa", result.getMessage());
     }
 
+    @Test
+    @DisplayName("Should Fail Transfer When Insufficient Funds")
+    public void shouldFailTransferWhenInsufficientFunds() {
+        // Given
+        transferService.executeTransfer(new TransferRequest("123456789", 500.0, "Initial transfer"));
+        TransferRequest request = new TransferRequest("123456789", 700.0, "Test transfer");
 
+        // When
+        TransferResult result = transferService.executeTransfer(request);
+
+        // Then
+        assertFalse(result.isSuccess());
+        assertEquals(500.0, result.getRemainingBalance());
+        assertEquals("Fondos insuficientes para realizar la transferencia", result.getMessage());
+    }
 }
