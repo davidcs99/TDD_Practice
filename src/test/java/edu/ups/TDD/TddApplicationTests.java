eaa7a5612b610562ffc76bd687ffae92eb8b622c
davidcs99 <luis.cs99.dc@gmail.com>
2024-12-29 21:10:42 -0500

    test: TRANSFER_AC1 desc: Should Transfer Successfully When Enough Funds phase: fail result: C:\Users\luisc\Documents\Maestria\automatizacion de los prrocesos\TDD\TDD\TDD\src\test\java\edu\ups\TDD\services\TransferServiceTest.java:20:9         java: cannot find symbol           symbol:   class TransferRequest           location: class edu.ups.TDD.services.TransferServiceTest         C:\Users\luisc\Documents\Maestria\automatizacion de los prrocesos\TDD\TDD\TDD\src\test\java\edu\ups\TDD\services\TransferServiceTest.java:20:39         java: cannot find symbol           symbol:   class TransferRequest           location: class edu.ups.TDD.services.TransferServiceTest         C:\Users\luisc\Documents\Maestria\automatizacion de los prrocesos\TDD\TDD\TDD\src\test\java\edu\ups\TDD\services\TransferServiceTest.java:23:9         java: cannot find symbol           symbol:   class TransferResult           location: class edu.ups.TDD.services.TransferServiceTest

---
+++ src/test/java/edu/ups/TDD/TddApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ups.TDD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+
+class TddApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+}
---
+++ src/test/java/edu/ups/TDD/services/TransferServiceTest.java
@@ -0,0 +1,32 @@
+package edu.ups.TDD.services;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TransferServiceTest {
+
+    private TransferService transferService;
+
+
+    @Test
+    @DisplayName("Should Transfer Successfully When Enough Funds")
+    public void shouldTransferSuccessfullyWhenEnoughFunds() {
+        // Given
+        TransferRequest request = new TransferRequest("123456789", 500.0, "Test transfer");
+
+        // When
+        TransferResult result = transferService.executeTransfer(request);
+
+        // Then
+        assertTrue(result.isSuccess());
+        assertEquals(500.0, result.getRemainingBalance());
+        assertEquals("Transferencia exitosa", result.getMessage());
+    }
+
+
+}
